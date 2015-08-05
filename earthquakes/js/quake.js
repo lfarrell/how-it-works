11ad0ff6c15bf74b9ab19ae114e163fce40a12cb
lfarrell <farrelldlfarrell@gmail.com>
2015-08-05 14:32:16 -0400

    Add note from where quake energy equation is from

--- earthquakes/js/quake.js
+++ earthquakes/js/quake.js
@@ -2,12 +2,23 @@ var stack = stack()
     .on("activate", activate)
     .on("deactivate", deactivate);
 
-var quake_energy = function(larger_quake, smaller_quake) {
+/**
+ * Calculations from http://earthquake.usgs.gov/learn/topics/how_much_bigger.php
+ * @param larger_quake
+ * @param smaller_quake
+ * @returns {number}
+ */
+var quake_energy_released = function(larger_quake, smaller_quake) {
     return Math.round(Math.pow(10, 1.5 * (larger_quake - smaller_quake)));
 };
 
-console.log(quake_energy(8.7, 5.8))
+console.log(quake_energy_released(8.7, 5.8))
 
+/**
+ *
+ * @param d
+ * @param i
+ */
 function activate(d, i) {
     if (i === followIndex) mystack.on("scroll.follow", refollow);
     if (i === lorenzIndex) startLorenz();
