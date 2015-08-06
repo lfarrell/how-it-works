bd0b3d2c22f7679d6f1ae9ebd1c07519511e8597
lfarrell <farrelldlfarrell@gmail.com>
2015-08-05 22:06:59 -0700

    Quake energy circles line up.

--- earthquakes/js/quake.js
+++ earthquakes/js/quake.js
@@ -1,7 +1,12 @@
-var width = document.body.clientWidth(),
-    height = document.body.clientHeight();
+var width = window.innerWidth,
+    height = window.innerHeight;
+
+var stack = stack()
+    .on("activate", activate)
+    .on("deactivate", deactivate);
 
 var section = d3.selectAll("section"),
+    intro = d3.select("#intor"),
     wave = d3.select("#waves"),
     type = d3.select("#types"),
     energy = d3.select("#energy"),
@@ -15,9 +20,7 @@ var section = d3.selectAll("section"),
     energyInterval,
     tsunamiInterval;
 
-var stack = stack()
-    .on("activate", activate)
-    .on("deactivate", deactivate);
+d3.selectAll('svg').attr('width', width).attr('height', 500);
 
 /**
  * Earthquake waves
@@ -52,7 +55,7 @@ var stopType = function() {
 var energy_attributes = {
     r: 5,
     cx: 20,
-    cy: 80,
+    cy: 100,
     transform: "translate(" + width/8 + "," + height/8 + ")"
 };
 
@@ -67,6 +70,8 @@ var quake_energy_released = function(larger_quake, smaller_quake) {
 };
 
 var energy_svg = d3.select("#energy svg g");
+var five_five_text = d3.select('#text_55');
+var six_text = d3.select('#text_6');
 
 var startEnergy = function () {
     for(var i=0; i<3; i++) {
@@ -76,25 +81,35 @@ var startEnergy = function () {
     }
 
     // 5.5 quake
+    five_five_text.classed('hide', false);
+
     d3.select('#energy_1, #energy_2').transition()
-        .delay(6000)
+        .delay(1000)
         .duration(1000)
         .attr({
-            'r': quake_energy_released(5.5, 5.0),
+            'r': quake_energy_released(5.5, 5.0) * 5,
+            'cx': 40,
+            'cy': 80,
             "transform": "translate(" + width/6 + "," + height/6 + ")"
         });
 
     // 6.0 quake
-    d3.select('#_energy_2').transition()
-        .delay(10000)
+    six_text.classed('hide', false);
+
+    d3.select('#energy_2').transition()
+        .delay(3000)
         .duration(1000)
         .attr({
-            'r': quake_energy_released(6.0, 5.0),
-            "transform": "translate(" + width/4 + "," + height/4 + ")"
+            'r': quake_energy_released(6.0, 5.0) * 5,
+            'cx': 240,
+            'cy': 0,
+            "transform": "translate(" + width/5 + "," + height/3.5 + ")"
         });
 };
 
 var stopEnergy = function() {
+    five_five_text.classed('hide', true);
+    six_text.classed('hide', true);
     energy_svg.stop();
 };
 
@@ -121,12 +136,13 @@ var startTsunami = function() {
 
     var circle_zero = d3.select('#tsunami_circle_zero');
     circle_zero.style('fill', 'none');
+
     d3.select('#tsunami_circle_one').style('fill', 'redbrick');
 
     tsunamiInterval = setInterval(function() {
         if(timer < 16) {
             circle_zero.attr('r',  radius + timer);
-            d3.select('#clock').text(timer + ':00');
+            d3.select('#clock').text(timer);
         }
         timer++;
     }, 1000);
